c3e8899137b7fcfe1d974dbade09d06f8d94d8c1
SpencerSedano <spencer.sv20@gmail.com>
2024-10-25 22:41:09 +0800

    created navbar compoment

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import BurgerMenu from "@/app/svg/burgerMenu.svg";
+import X from "@/app/svg/x.svg";
+
+export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState(true);
+
+  function handleOpenMenu() {
+    setOpenMenu(!openMenu);
+    console.log("you clicked it");
+  }
+
+  return (
+    <>
+      <nav className="w-full h-24 text-2xl m-4 flex items-center">
+        {openMenu ? (
+          <Image
+            className="w-8 sm:hidden"
+            src={BurgerMenu}
+            alt="Burger Menu"
+            priority
+            onClick={handleOpenMenu}
+          />
+        ) : (
+          <>
+            <ul className="sm:hidden">
+              <Image
+                className="w-8 sm:hidden"
+                src={X}
+                alt="X Menu"
+                onClick={handleOpenMenu}
+              />
+              <li>
+                <a href="/">FitIn</a>
+              </li>
+              <li>
+                <a href="/start">Start</a>
+              </li>
+              <li>
+                <a href="/login">Login</a>
+              </li>
+            </ul>
+          </>
+        )}
+        <ul className="hidden sm:w-full sm:flex sm:justify-evenly">
+          <li>
+            <a href="/">FitIn</a>
+          </li>
+          <li>
+            <a href="/start">Start</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
